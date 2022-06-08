fc502b3aae3d6925a9e1ce531f48a93967c6b2f3
JamesNg <njames15@gmail.com>
2022-06-07 21:07:23 -0400

    commit tree 70e144f695c75ad70900b09a4d62a40849607a33

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+class Anagram
+    attr_accessor :name
+
+    def initialize(word)
+        @name = word
+    end
+
+    def match(array_words)
+        # word_chars = word.chars.sort {|a,b| a<=>b}
+        # words_array = array_words.map{|word| word.chars.sort{|word1, word2| word1 <=> word2}}
+        # words_array.find {|word| word == words.chars}]
+        array_words.filter {|word| word.chars.sort == @name.chars.sort}
+    end
+
+end
+
+apple = Anagram.new('apple')
+puts apple.name
+puts apple.match(%w[the big ape])
